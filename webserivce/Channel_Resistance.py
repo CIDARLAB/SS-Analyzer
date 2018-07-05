3f960488e93ed71e95d6379ebbdb1b3c67859018
samperid <samperid@bu.edu>
2018-07-05 11:25:37 -0400

    Added Channel Resistance Model

--- webserivce/Channel_Resistance.py
+++ webserivce/Channel_Resistance.py
@@ -4,26 +4,24 @@ from math import *
 W_viscosity = 8.9*pow(10,-4)
 #milli = pow(10,-3)
 micro = pow(10,-6)
-test = True
 
-#width = input("Insert width: ")
-#depth = input("Insert depth: ")
+#include depth input after JSON fixed
+def Channel_R(channelWidth,depth):
 
-#include length input after JSON is fixed 
-def Channel_R(width, depth):
 
-    #Reassign inputs as float 
-    width = float(width)
+    width = float(channelWidth)
     depth = float(depth)
-    #length = float(length)
+    
+    #HARD CODED VALUE OF LENGTH
     length = 2000.00
+
     width = width*micro
     depth = depth*micro
     length = length*micro
 
-    alpha = 12*(1*pow(1-((192*depth)/(pow(pi,5)*width))*tanh((pi*width)/(2*depth)),-1))
+    alpha = 12*pow(1-((192*depth)/(pow(pi,5)*width))*tanh((pi*width)/(2*depth)),-1)
     resistance = (alpha*W_viscosity*length)/(width*pow(depth,3))
 
     return resistance
 
-#print(Channel_R(width,depth))
+#print(Mixer_R(bendSpacing,numberOfBends,channelWidth,bendLength))
