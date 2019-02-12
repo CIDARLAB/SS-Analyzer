3f989d8d855c4398c5cfe8b34a03e3c1cc658b71
Radhakrishna Sanka <rkrishnasanka@gmail.com>
2019-02-11 23:26:27 -0500

    Rmoved the old parser

--- parser/target.py
+++
@@ -1,9 +0,0 @@
-class Target:
-
-    def __init__(self,component,port):
-        self.component = component
-        self.port = port
-
-    def __str__(self):
-        return str(self.__dict__)
-        
