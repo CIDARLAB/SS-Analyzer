9f9f8e272cd8995020c7c25eaa8471c87b18ea72
Radhakrishna Sanka <rkrishnasanka@gmail.com>
2019-02-13 16:43:37 -0500

    Updated the model to look up against ids instead of names

--- config.py
+++ config.py
@@ -23,20 +23,27 @@ def getInletOutlet(name):
 def getInletsAndOutlets():
     return inletOutletDict
 
-def parseConfig(file):
+def getIDForName(name, device):
+    components = device.getComponents()
+    for component in components:
+        if component.name == name:
+            return component.ID
+
+def parseConfig(file, device):
 
     lines = file.read().splitlines()
     for line in lines: 
         line = line.strip()
         parts = line.split(',')
         name = parts[0].strip()
+        id = getIDForName(name, device)
         state = parts[1].strip()
         value = parts[2].strip()
         setInletOutlet(name, state)
         if state == "IN":
-            setFlowRate(name, float(value))
+            setFlowRate(id, float(value))
         elif state == "OUT":
-            setPressure(name, float(value))
+            setPressure(id, float(value))
         else:
             print('Unkown state:', state)
     file.close()
--- tool.py
+++ tool.py
@@ -16,12 +16,17 @@ argparser.add_argument("-i","--input", help="Config file")
 args = argparser.parse_args()
 
 file_c = open(args.input, 'r')
-parseConfig(file_c)
-
 file_d = open(args.design, 'r')
+
 JSON = json.loads(file_d.read())
 
 device = Device(JSON)
+
+parseConfig(file_c, device)
+
+print("Printing the Micrfluidic Network's edges:")
+for edge in device.G.edges():  
+    print("Printing Edge :", edge)
 
 electrical_network = ENetwork(device)
 
